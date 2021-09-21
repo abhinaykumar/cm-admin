4b4f0bb1c7cdd784aeee6276300883975ffa7ea6
ADITYA TIWARI <taditya.tiwari007@gmail.com>
2021-09-21 19:55:37 +0530

    Refactor: Create sortable list if element present

--- app/javascript/packs/cm_admin/scaffolds.js
+++ app/javascript/packs/cm_admin/scaffolds.js
@@ -7,10 +7,13 @@ $(document).ready(function(e) {
   flatpickr("[data-behaviour='filter'][data-filter-type='date']", {
     mode: 'range'
   })
-  Sortable.create($('.columns-list')[0],{
-    handle: '.dragger',
-    animation: 150
-  });
+  if ($('.columns-list').length > 0) {
+    Sortable.create($('.columns-list')[0],{
+      handle: '.dragger',
+      animation: 150
+    });
+  }
+
 });
 
 $(document).on("keypress keyup blur", "[data-behaviour='decimal-only'], [data-behaviour='filter'][data-filter-type='range']", function (e) {
